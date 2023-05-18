40d174b695a248ec489644a8f2220f51be1bacad
HONGJAE0130 <76721659+HONGJAE0130@users.noreply.github.com>
2023-05-18 11:04:37 +0900

    feat:Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,68 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
-import * as _ from 'lodash';
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+exports.handler = async (event, context) => {
+  try {
+    // 기본적으로 모든 playerNAME 항목의 속성 값과 profile_photo_url 값을 조회
+    const getAllItems = async () => {
+      const params = {
+        TableName: 'nba-info'
+      };
 
-export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-    const max = 10;
-    const val = _.random(max);    
-    const response = {
-        statusCode: 200, 
-        body: `The random value (max ${max}) is: ${val}`,
+      const result = await dynamoDB.scan(params).promise();
+      return result.Items.map(item => ({
+        id: item.id,
+        name: item.playerNAME,
+        bodyShotUrl: item.body_shot_url,
+        imageUrl: item.profile_photo_url
+      }));
     };
-    return response;
+
+    // 사용자가 입력한 값으로 필터링하여 결과 반환
+    const getFilteredItems = async (name) => {
+      const params = {
+        TableName: 'nba-info',
+        FilterExpression: 'contains(playerNAME, :name)',
+        ExpressionAttributeValues: {
+          ':name': name
+        }
+      };
+
+      const result = await dynamoDB.scan(params).promise();
+      return result.Items.map(item => ({
+         id: item.id,
+        name: item.playerNAME,
+        bodyShotUrl: item.body_shot_url,
+        imageUrl: item.profile_photo_url
+      }));
+    };
+
+    // 사용자가 입력한 name 값
+    const name = event.queryStringParameters && event.queryStringParameters.name;
+
+    let items;
+
+    // name 값이 없으면 모든 항목을 조회하여 반환
+    if (!name) {
+      items = await getAllItems();
+    } else {
+      // name 값이 존재하면 필터링하여 반환
+      items = await getFilteredItems(name);
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,GET"
+      },
+      body: JSON.stringify(items)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message)
+    };
+  }
 };
